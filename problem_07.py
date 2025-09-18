1dc04a10a8ccf462134f08c6eb812b3491431210
D.G.S.S.Dhananjana <149720690+dhananjana998@users.noreply.github.com>
2025-09-18 22:53:30 +0530

    problem_07.py

---
+++ problem_07.py
@@ -0,0 +1,18 @@
+"""
+
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13. What is the 10,001st prime number?
+"""
+from sympy import prime
+
+
+print("10,001 prime:", prime(10001))
+
+"""
+output:
+
+10,001 prime: 104743
+
+
+
+"""
